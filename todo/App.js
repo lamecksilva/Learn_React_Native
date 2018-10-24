7d2042dd3c094a4435d2887cd094481a8c3596da
lamecksilva <lameck2@hotmail.com>
2018-10-24 07:55:12 -0300

    Era pra ter feito esse commit umas 2 semanas atrás.....

--- todo/App.js
+++ todo/App.js
@@ -5,30 +5,53 @@ import { green } from "ansi-colors";
 class App extends React.Component {
   state = {
     text: "",
-    todo: ["Learn react", "Learn react native", "react apps"]
+    todo: []
   };
 
   addTodo = () => {
-    this.setState({ todo: this.state.text });
+    var newTodo = this.state.text;
+    var arr = this.state.todo;
+    arr.push(newTodo);
+    this.setState({ todo: arr, text: "" });
   };
 
   renderTodos = () => {
     return this.state.todo.map(t => {
-      return <Text key={t}>{t}</Text>;
+      return (
+        <Text
+          style={{ fontSize: 25 }}
+          onPress={() => {
+            this.deleteTodo(t);
+          }}
+          key={t}
+        >
+          {t}
+        </Text>
+      );
     });
+  };
+
+  deleteTodo = t => {
+    var arr = this.state.todo;
+    var pos = arr.indexOf(t);
+    arr.splice(pos, 1);
+    this.setState({ todo: arr });
   };
 
   render() {
     return (
       <View style={styles.viewStyle}>
-        <Text>Hello World, this app is great</Text>
-        <TextInput
-          style={styles.inputStyle}
-          underlineColorAndroid="transparent"
-          onChangeText={text => this.setState({ text })}
-        />
-        <Button title="Add Todo" color="green" onPress={this.addTodo} />
-        {this.renderTodos()}
+        <View style={styles.container}>
+          <Text style={styles.title}>Todo App</Text>
+          <TextInput
+            style={styles.inputStyle}
+            underlineColorAndroid="transparent"
+            onChangeText={text => this.setState({ text })}
+            value={this.state.text}
+          />
+          <Button title="Add Todo" color="green" onPress={this.addTodo} />
+          {this.renderTodos()}
+        </View>
       </View>
     );
   }
@@ -37,11 +60,17 @@ class App extends React.Component {
 const styles = {
   viewStyle: {
     padding: 10,
-    justifyContent: "center",
-    alignItems: "center",
     flex: 1
   },
+  title: {
+    fontSize: 40,
+    textAlign: "center"
+  },
+  container: {
+    paddingTop: "4%"
+  },
   inputStyle: {
+    padding: 10,
     height: 40,
     width: "100%",
     borderColor: "green",
